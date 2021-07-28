68e84ea19ffc4bedb02d1b35f9d4479fa8c1cc7b
maitandat1507 <maitandat1507@gmail.com>
2021-07-29 00:20:58 +0700

    adding ref link for previous commit

--- basics-10-styling-starting-setup/app.js
+++ basics-10-styling-starting-setup/app.js
@@ -6,6 +6,9 @@ const app = Vue.createApp({
   },
   methods: {
     toggleBox(i) {
+
+      // dynamic variable
+      // @ref: https://stackoverflow.com/questions/12149233/concatenate-a-dynamic-variable-name-in-javascript
       this.a['selectedBox' + i] = !this.a['selectedBox' + i]
     },
   }

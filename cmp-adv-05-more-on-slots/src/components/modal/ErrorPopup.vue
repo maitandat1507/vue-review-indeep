a6bbff934b4722d8f7f3a774fdb8d65d911c9aed
maitandat1507 <maitandat1507@gmail.com>
2021-07-29 16:06:48 +0700

    error-popup w slot

---
+++ cmp-adv-05-more-on-slots/src/components/modal/ErrorPopup.vue
@@ -0,0 +1,22 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<script>
+export default {
+  methods: {
+    //
+  }
+}
+</script>
+
+<style scoped>
+dialog {
+  margin: 0 auto;
+  width: 200px;
+  height: 100px;
+  text-align: center;
+}
+</style>
--- cmp-adv-05-more-on-slots/src/components/tab/Tab1.vue
+++ cmp-adv-05-more-on-slots/src/components/tab/Tab1.vue
@@ -1,17 +1,35 @@
 <template>
   <h3>{{tab_title}}</h3>
   <input type="text" v-model="name">
+  <button @click="verify()">Submit</button>
+  <error-popup v-if="modalActive">
+    <h3>Must type name!</h3>
+    <button @click="modalActive = false">OK</button>
+  </error-popup>
 </template>
 
 <script>
-// import { defineComponent } from '@vue/composition-api'
+import ErrorPopup from '../modal/ErrorPopup'
 
 export default {
+  components: {
+    ErrorPopup,
+  },
   data() {
     return {
       name: '',
-      tab_title: "This is Tab 1"
+      tab_title: "This is Tab 1",
+      modalActive: false,
     }
+  },
+  methods: {
+    verify() {
+      if (this.name === '') {
+        return this.modalActive = true
+      } else {
+        return this.modalActive = false
+      }
+    },
   }
 }
 </script>
